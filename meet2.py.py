cb7cfc6eb6cd673524b24dd147f6158f09e748de
Kholqi_Alprog-C <kholqimulyaahmadi16@gmail.com>
2025-10-30 21:12:18 +0800

    Add files via upload

---
+++ meet2.py.py
@@ -0,0 +1,36 @@
+# Variabel adalah cara kita menyampaikan/mendeklarasikan nilai
+
+# memberikan nilai/ asigment
+x = 10
+y = 6
+lebar = 50
+
+print("nilai dari x=", x)
+print("nilai dari y=", y)
+print("nilai dari lebar=", lebar)
+
+# penamman variabel
+jumlah_z =9 #lebih dari satu kata, maka hanya bisa digabung dengan underscore
+bet99 = 100000 #tidak oleh di awali dengan angka
+jumlahZ = 10
+
+#pemanggilan kedua 
+print("nilai dari x=", x)
+x = 99
+print("nilai dari x=", x)
+
+
+# pemanggilan indirect
+a = x
+b = lebar
+print("nilai a =", a)
+print("nilai b=", b)
+
+#tugas:
+# nama saya = {nama kalian}
+# nim saya = {nim kalian}
+# buat program agar muncul di layar
+
+# jawab
+print ("nama saya adalah = kholqi mulya ahmadi")
+print ("nim saya adalah = 25241079")
