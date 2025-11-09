eb855cbc6278e061604ccb4e6ea0f73b409f3030
Kholqi_Alprog-C <kholqimulyaahmadi16@gmail.com>
2025-11-09 08:28:45 +0800

    Add files via upload

---
+++ meet3.py
@@ -0,0 +1,18 @@
+# will will lear about data type
+
+x = 10 # bilangan bulat (integer)
+print("nilai dari x =" ,)
+print("tipe data dari x =" ,)
+
+
+y = 12,5 # bilangan pecahan (float)
+print("nilai dari y =" ,y)
+print("type data dari y =" ,type(y))
+
+ukuran = "xxl" # tipe data (string)
+print("nilai dari ukuran =" ,ukuran)
+print ("tipe data dari ukuran=" ,type (ukuran))
+
+kebenaran =True # tipe data boolean (true false)
+print("nilai dari kebenaran =" , kebenaran)
+print("tipe data dari kebenaran =" ,type (kebenaran))
